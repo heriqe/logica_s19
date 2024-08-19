c60c6d84bc30b72e524c8a53936b440aa08eaf70
Sérgio Carvalho <sergiocarvalho.m@outlook.com>
2024-08-19 12:42:46 -0300

    Init Git

---
+++ calc1.py
@@ -0,0 +1,24 @@
+# Exercício - Código para calcular gastos
+
+gabriel = {
+    'nome': 'Gabriel',
+    'gastos': [49, 200, 9.99]
+}
+
+carol = {
+    'nome': 'Carol',
+    'gastos': [300, 420, 59.90, 33.80]
+}
+
+joao = {
+    'nome': 'João',
+    'gastos': [39.90, 23.50, 6.10, 19.50]
+}
+
+pessoas = [gabriel, carol, joao]
+
+# Implemente um loop for 
+# para exibir por pessoa o:
+# total gasto
+# o menor valor gasto
+# o maior valor gasto
---
+++ while_else.py
@@ -0,0 +1,15 @@
+# Exercício Python: While com Else
+# Crie um laço loop while que se repete 
+# enquanto uma determinada condição for verdadeira.
+
+# O loop while deve ter um else para 
+# executar quanto o loop while for finalizado 
+# normalmente.
+
+# A implementação deve pedir por um número 
+# maior que 1 e menor ou igual 100 e verificar 
+# se a condição é verdadeira, 
+# faça um loop while para verificar também 
+# se o número é um número primo.
+
+# Atenção: não faça uma condição loop `divisor <= numero`
